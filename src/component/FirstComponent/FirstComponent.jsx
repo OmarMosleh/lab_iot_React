c84b715a63489d7365b65b76713e6f1c7032419d
Omar_Mosleh <omarmosleh90@gmail.com>
2023-10-13 16:09:13 +0200

    firstLab

---
+++ src/component/FirstComponent/FirstComponent.jsx
@@ -0,0 +1,9 @@
+import react from 'react';
+
+class FirstComponent extends react.Component{
+    render(){
+     return   <h1>hi from class component </h1>;
+    }
+}
+
+export default FirstComponent ;
---
+++ src/component/SecondComponent/SecondComponent.jsx
@@ -0,0 +1,5 @@
+function SecondComponent() {
+    return <h1>Hi from function component</h1>;
+}
+
+export default SecondComponent;
